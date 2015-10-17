4dd481ec27d2599f8cbb788d4e5b5220838158a8
AdeB <adbrebs@gmail.com>
2015-10-17 16:46:37 -0400

    Modify rnn code so that the hidden state is kept alive between batches

--- main.py
+++ main.py
@@ -14,25 +14,29 @@ if __name__ == '__main__':
     emb_size = 20
     n_hidden = 500
     lr = 0.1
+    print_frequency = 10000
 
     # Dataset
     ds = CharDataset(textfile, batch_size, seq_len, force_reset_data=False)
     size_dict = len(ds.dictionary)
 
     # Model
-    # m = FF(seq_len, emb_size, n_hidden, size_dict, lr)
-    m = RNN(seq_len, emb_size, n_hidden, size_dict, lr)
+    m = FF(seq_len, emb_size, n_hidden, size_dict, lr)
+    # m = RNN(seq_len, emb_size, n_hidden, size_dict, batch_size, lr)
 
     # Training
     last = time.clock()
+    cost = .0
     for i in range(100000000):
         batch, target = ds.next_batch()
 
-        c = m.fun_cost(batch, target)
-        if not i % 1000:
+        cost += m.train(batch, target, i)
+        if not i % print_frequency:
             n = time.clock()
-            print 'Iteration {}, time {}, cost {}'.format(i, n - last, c)
+            print 'Iteration {}, time {}, cost {}'.format(
+                i, n - last, cost/print_frequency)
+            cost = 0
             last = n
-            m.sample(ds.dictionary, ds.inv_dictionary)
+            m.sample(ds.dictionary, ds.inv_dictionary, seq_print_len=1000)
             print ""
             print "-"*70
--- model.py
+++ model.py
@@ -58,6 +58,9 @@ class FF:
         next_char = t.argmax(rng.multinomial(pvals=y_hat), axis=1)
         self.fun_predict = theano.function([x], next_char)
 
+    def train(self, batch, target, i):
+        return self.fun_cost(batch, target)
+
     def sample(self, dictionary, inv_dictionary,
                begin="MADAME PERNELLE", seq_print_len=1000):
         """
@@ -80,11 +83,12 @@ class FF:
 class RNN:
     """
     Recurrent neural network with an embedding layer, a hidden layer of
-    relu IRNN and a softmax output layer.
+    relus and a softmax output layer.
     """
-    def __init__(self, seq_len, emb_size, n_hidden, size_dict, lr):
+    def __init__(self, seq_len, emb_size, n_hidden, size_dict, batch_size, lr):
 
         self.seq_len = seq_len
+        self.batch_size = batch_size
 
         w_emb = shared(np.random.normal(
             0, 0.01, size=(size_dict, emb_size)).astype(dtype=floatX))
@@ -96,7 +100,9 @@ class RNN:
             0, 0.01, size=(n_hidden,)).astype(dtype=floatX))
 
         # IRNN initialization
-        w_hidden = shared(np.eye(n_hidden).astype(dtype=floatX))
+        # w_hidden = shared(np.eye(n_hidden).astype(dtype=floatX))
+        w_hidden = shared(np.random.normal(
+            0, 0.01, size=(n_hidden, n_hidden)).astype(dtype=floatX))
 
         b_hidden = shared(np.random.normal(
             0, 0.01, size=(n_hidden,)).astype(dtype=floatX))
@@ -111,9 +117,8 @@ class RNN:
         x = t.imatrix('x')
         y = t.ivector('y')
 
-        init_state = shared(np.random.normal(
-            0, 0.01, size=(n_hidden,)).astype(dtype=floatX))
-        buff = init_state
+        self.init_state = shared(np.zeros((batch_size, n_hidden), dtype=floatX))
+        buff = self.init_state
         for e in xrange(seq_len):
             emb = w_emb[x[:, e]]
             emb = emb.reshape((x.shape[0], -1))
@@ -123,10 +128,11 @@ class RNN:
 
         cost = t.nnet.categorical_crossentropy(y_hat, y).mean()
 
-        params = [init_state, w_emb, w_in, w_hidden, b_hidden, w_out, b_out]
+        params = [w_emb, w_in, w_hidden, b_hidden, w_out, b_out]
 
         grads = t.grad(cost, params)
-        updates = [(w, w - lr * p) for w, p in zip(params, grads)]
+        updates = [(self.init_state, buff)] + \
+                  [(w, w - lr * p) for w, p in zip(params, grads)]
 
         self.fun_cost = function([x, y], cost, updates=updates)
 
@@ -134,20 +140,36 @@ class RNN:
         next_char = t.argmax(rng.multinomial(pvals=y_hat), axis=1)
         self.fun_predict = function([x], next_char)
 
+    def train(self, batch, target, i):
+        # Once in a while, we reset the initial set
+        if i % 1000:
+            self.init_state.set_value(
+                np.zeros_like(self.init_state.get_value()))
+        return self.fun_cost(batch, target)
+
     def sample(self, dictionary, inv_dictionary,
                begin="MADAME PERNELLE", seq_print_len=1000):
         """
         Sample from the model.
         seq_print_len is the length of the sequence to be printed.
         """
+
         begin = begin[:self.seq_len]
 
+        # Save the state of the model and set it to zero
+        saved_state = self.init_state.get_value()
+        self.init_state.set_value(np.zeros_like(saved_state))
+
         prev = np.array([dictionary[ch] for ch in begin], dtype='int16')
-        prev = prev[None, :]
+        prev = np.repeat(prev[None,:], self.batch_size, axis=0)
 
         print begin,
         for i in xrange(seq_print_len):
             a = self.fun_predict(prev)[0]
             sys.stdout.write(inv_dictionary[a])
-            prev = np.roll(prev, -1)
+            prev = np.roll(prev, -1, axis=1)
             prev[:, -1] = a
+
+        # Restore training state
+        self.init_state.set_value(saved_state)
+
